c9f182ad8b09ed70a9e16f646d27ec552d14d29d
arthur-1205 <anh1252001@gmail.com>
2022-07-03 17:43:35 +0700

    test

--- DoanAnh/Task6_Script/task6_1.sh
+++ DoanAnh/Task6_Script/task6_1.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-install_nginx_as_rp(){
-# Configuration
-     domain=""
-     PROXY_HOST=""
-     PROXY_USER=""
-     PROXY_PASSWORD=""
-     PROXY_PORT=""
-     block="/etc/ngix/sites-available/$domain"
+install_nginx_as_rp() {
+  # Configuration
+  domain=""
+  PROXY_HOST=""
+  PROXY_USER=""
+  PROXY_PASSWORD=""
+  PROXY_PORT=""
+  block="/etc/ngix/sites-available/$domain"
 
-     echo "[ Setup proxy ]"
-     echo -n "Domain:"
-     read domain
-     echo -n "Host: "
-     read PROXY_HOST
-     echo -n "Port: "
-     read PROXY_PORT
-     echo -n "Username: "
-     read PROXY_USER
-     echo -n "Password: "
-     read -s PROXY_PASSWORD
-     echo "Updating home dir"
-     sudo mkdir /home/$PROXY_USER
+  echo "[ Setup proxy ]"
+  echo -n "Domain:"
+  read domain
+  echo -n "Host: "
+  read PROXY_HOST
+  echo -n "Port: "
+  read PROXY_PORT
+  echo -n "Username: "
+  read PROXY_USER
+  echo -n "Password: "
+  read -s PROXY_PASSWORD
+  echo "Updating home dir"
+  sudo mkdir -p /home/$PROXY_USER
 
-     echo_statement "Configuring NGINX reverse proxy server"
-     sudo apt-get install epel-realease nginx
-     sudo apt-get install nginx-full -y
+  echo_statement "Configuring NGINX reverse proxy server"
+  sudo apt-get install epel-realease nginx
+  sudo apt-get install nginx-full -y
 
- # Set apt proxy settings
-"server {
+  # Set apt proxy settings
+  "server {
       listen $PROXY_PORT;
       server_name $domain;
       access_log /var/log/nginx/access.log;
@@ -37,96 +37,94 @@ install_nginx_as_rp(){
       proxy_set_header X-Forwarded-For '$proxy_add_x_forwarded_for';
 
       location / {
-           proxy pass http://$PROXY_IP:$PROXY_POST;
+           proxy pass http://$PROXY_IP:$PROXY_PORT;
       }
  }
 "
 
- echo "$apt_conf_proxy" | sudo tee -a $block > /dev/null
+  echo "$apt_conf_proxy" | sudo tee -a $block >/dev/null
 
- echo ""
- sudo ln -s $block /etc/nginx/sites-enabled/
- 
- echo "Reload the Server"
- sudo nginx -t && sudo service nginx reload
- 
- echo "Proxy enabled."
+  echo ""
+  sudo ln -s $block /etc/nginx/sites-enabled/
+
+  echo "Reload the Server"
+  sudo nginx -t && sudo service nginx reload
+
+  echo "Proxy enabled."
 }
 
+nginx_reverse_proxy_check() {
+  URL=
+  echo "Input URL of website to check Reverse Proxy: "
+  read URL
+  RESPONSE_MESSAGE=$(curl -S -I ${URL} 2>/dev/null | grep "Server" | awk '{print $2}')
+  if [ -z "$RESPONSE_MESSAGE" ]; then
+    echo "Server not installed Reverse Proxy yet"
+    install_nginx_as_rp
 
-nginx_reverse_proxy_check(){
-        echo "Input URL of website to check Reverse Proxy: "
-        read URL
-        RESPONSE_MESSAGE=`curl -S -I ${URL} 2> /dev/null | grep "Server" | awk '{print $2}'`
-        if [ -z "$RESPONSE_MESSAGE" ] 
-        then
-            echo "Server not installed Reverse Proxy yet"
-            install_nginx_as_rp
-         
-        elif [ "$RESPONSE_MESSAGE"=="nginx"* ] 
-        then
-            echo "Server already installed Reverse Proxy - ${RESPONSE_MESSAGE}"
-           
-        else
-            echo "WARNING - Server not installed Nginx Reverse Proxy yet"
-            install_nginx_as_rp
-            
-        fi
+  elif [ "$RESPONSE_MESSAGE" == "nginx"* && "$RESPONSE_MESSAGE" == "Apache"]; then
+    echo "Server already installed Reverse Proxy "
+
+  else
+    echo "WARNING - Server not installed Nginx Reverse Proxy yet"
+    install_nginx_as_rp
+
+  fi
 }
 
+create_new_domain() {
+  name=
+  WEB_ROOT_DIR=
+  IP=
 
-create_new_domain(){
-name=
-WEB_ROOT_DIR=
-IP=
+  echo "Enter a name of Domain: "
+  read name
+  echo "Enter a root directory of Domain(/var/www/html/...): "
+  read WEB_ROOT_DIR
+  echo "Enter the IP of Domain: "
+  read IP
+  sitesEnable='/etc/httpd/sites-enabled/'
+  sitesAvailable='/etc/httpd/sites-available/'
+  sudo mkdir -p $sitesEnable
+  sudo mkdir -p $sitesAvailable
+  sitesAvailabledomain=$sitesAvailable$name.conf
+  sudo mkdir -p $sitesAvailabledomain
+  echo "Creating a vhost for $sitesAvailabledomain with a webroot $WEB_ROOT_DIR"
+  sed -i '$aIncludeOptional sites-enabled/*.conf' /etc/httpd/conf/httpd.conf
 
-echo "Enter a name of Domain"
-read name
-     echo "Enter a root directory of Domain: "
-     read WEB_ROOT_DIR
-     echo "Enter the IP of Domain: "
-     read IP
-
- sitesEnable='/etc/apache2/sites-enabled/'
- sitesAvailable='/etc/apache2/sites-available/'
- sitesAvailabledomain=$sitesAvailable$name.conf
- echo "Creating a vhost for $sitesAvailabledomain with a webroot $WEB_ROOT_DIR"
-
- ### create virtual host rules file
- echo "
+  ### create virtual host rules file
+  echo "
     <VirtualHost *:80>
       ServerName $name'$host';
       <Directory $WEB_ROOT_DIR/>
         Options Indexes FollowSymLinks
         AllowOverride all
       </Directory>
-    </VirtualHost>" > $sitesAvailabledomain
- echo -e $"\nNew Virtual Host Created\n"
+    </VirtualHost>" >$sitesAvailabledomain
+  echo -e $"\nNew Virtual Host Created\n"
 
- sudo tee -a $IP $name\n > /etc/hosts
+  sudo tee -a $IP $name\n >/etc/hosts
+  sudo systemctl restart httpd 2>/dev/null
 
- sudo a2ensite $name
- service apache2 reload
-
- echo "Done, please browse to http://$name to check!"       
+  echo "Done, please browse to http://$name to check!"
 
 }
 
-# Start_service 
-start_service(){
-sudo systemctl start apache2
-sudo systemctl enable apache2
-sudo systemctl start nginx
-sudo systemctl enable nginx
-sudo systemctl start mysqld 2> /dev/null
+# Start_service
+start_service() {
+  sudo systemctl start apache2
+  sudo systemctl enable apache2
+  sudo systemctl start nginx
+  sudo systemctl enable nginx
+  sudo systemctl start mysqld 2>/dev/null
 
 }
 function menu() {
-echo " ------------  Manage Domain Menu ----------- "
-PS3=" ==> Enter the option: "
-cal=("Install_service" "Start_service" "Secure_mysql" "Setup_vhost" "Create_vhost" "Exit_the_program")
-select i in "${cal[@]}" ; do
-  case $i in
+  echo " ------------  Manage Domain Menu ----------- "
+  PS3=" ==> Enter the option: "
+  cal=("Create new Domain" "Start_service" "Secure_mysql" "Setup_vhost" "Create_vhost" "Exit_the_program")
+  select i in "${cal[@]}"; do
+    case $i in
     ${cal[0]})
       echo "You chose Install create new Domain"
       create_new_domain
@@ -144,7 +142,7 @@ select i in "${cal[@]}" ; do
       echo "You chose Setup VHost "
       ;;
 
-    ${cal[4]}) 
+    ${cal[4]})
       echo "You chose Create VHost "
       ;;
 
@@ -153,11 +151,11 @@ select i in "${cal[@]}" ; do
       break
       ;;
 
-    *) 
+    *)
       echo "Option no valadition!!"
       ;;
-  esac
-done
+    esac
+  done
 }
 
 main() {
