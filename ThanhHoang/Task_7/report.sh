3c06d7f50c03ced0a588ea93a16e1e432a6f9849
SpaceNoShy <lethanhhoang1113@gmail.com>
2022-08-08 10:47:29 +0700

    updated

--- ThanhHoang/Task_7/report.sh
+++ ThanhHoang/Task_7/report.sh
@@ -15,36 +15,42 @@ install_service() {
 
     echo -e "\n\nInstalling Apache2 Web server\n"
     
-    sudo apt-get install apache2 apache2-utils 
+    sudo apt-get install apache2 apache2-utils -y
     sudo systemctl restart apache2
     sudo systemctl enable apache2
 
     echo -e "\n\nInstalling PHP & Requirements\n"
     sudo apt install software-properties-common apt-transport-https -y
     sudo add-apt-repository ppa:ondrej/php -y
+    sudo apt install -y php
+    sudo apt-get install php7.0-mysql php7.0-curl php7.0-cli -y 
     sudo apt update
     sudo apt upgrade
-    sudo apt install php7.4-fpm php7.4-common libapache2-mod-fcgid php7.4-cli
-    sudo a2enmod proxy_fcgi setenvif && sudo a2enconf php7.4-fpm
+    sudo apt install php7.0-fpm php7.0-common libapache2-mod-fcgid php7.0-cli -y
+    sudo a2enmod proxy_fcgi setenvif && sudo a2enconf php7.0-fpm
 
     echo -e "\n\nInstalling MySQL\n"
-    sudo apt-get install mysql-server mysql-client -y
+    #sudo apt-get install mysql-server mysql-client -y
+    sudo apt-get install mariadb-server -y
+    sudo systemctl enable mysql
+    sudo systemctl start mysql
+    service mysql status
 
     echo -e "\n\nRestarting Apache\n"
     sudo service apache2 restart
 
       echo -e "\n\nPermissions for /var/www\n"
-    sudo chmod -R 0755 /var/www/html/
+    sudo chmod -R 755 /var/www/html/
     sudo chown -R www-data:www-data /var/www/
     echo -e "\n\n Permissions have been set\n"
 
     echo -e "\n\nInstall Nginx Reverse Proxy Server\n"
-    sudo apt-get install nginx
+    sudo apt install nginx -y 
     sudo ufw disable
 
     echo 'Finally Checking status of services'
-    echo Apache service is $(systemctl show -p ActiveState --value apache2)
-    echo MYSQL service is $(systemctl show -p ActiveState --value mysqld)
+    echo Apache service is $(systemctl status apache2)
+    echo MYSQL service is $(systemctl status mysqld)
 
     exit 0
 
@@ -75,51 +81,6 @@ Install_Proxy(){
  sudo ln -s $block /etc/nginx/sites-enabled/
  
  sudo nginx -t && sudo service nginx reload
-}
-
-delete_old_domain(){
-     if [ -z $DOMAIN ]
-     then
-  	     echo "You did not include the domain ..."
-  	     exit 1
-     fi
-
-     sudo rm -rf /home/$USERNAME/$DOMAIN
-     sudo rm -rf $APACHEDIR/sites-available/$DOMAIN
-}
-
-create_folder_new_domain(){
-
-     read DOMAIN
-
- ## Make sure a domain was passed
-     if [ -z $DOMAIN ]
-     then
-  	     echo "You did not include the new domain name..."
-  	     exit 1
-     fi
-
- ## Create the directory for the new domain
- mkdir -p /home/$USERNAME/$DOMAIN
- echo "+ Created dir /home/$USERNAME/$DOMAIN"
-
- ## Copy the vhosts template
- sudo mkdir -p $APACHEDIR/sites-available/$DOMAIN
- echo "+ Created vhosts file at $APACHEDIR R/sites-available/$DOMAIN"
-
- ## chown/chmod the vhosts files
- sudo chown $USERNAME:$USERGROUP $APACHEDIR/sites-available/$DOMAIN
- sudo chmod 775 $APACHEDIR/sites-available/$DOMAIN
- echo "+ chowned/chmoded vhosts file"
-
- sudo ln -s $APACHEDIR/sites-available/$DOMAIN $APACHEDIR/sites-enabled/
-
- echo "+ Enabling the domain and reloading Apache:"
- sudo a2ensite $DOMAIN
- sudo service apache2 reload
-
- echo "! Finished! Domain \"$DOMAIN\" was added"
-     
 }
 
 create_new_domain(){
@@ -184,6 +145,73 @@ create_user_group(){
 		
 }
 
+create_folder_new_domain(){
+
+     read DOMAIN
+
+ ## Make sure a domain was passed
+     if [ -z $DOMAIN ]
+     then
+  	     echo "You did not include the new domain name..."
+  	     exit 1
+     fi
+
+ ## Create the directory for the new domain
+ mkdir -p /home/$USERNAME/$DOMAIN
+ echo "+ Created dir /home/$USERNAME/$DOMAIN"
+
+ ## Copy the vhosts template
+ sudo mkdir -p $APACHEDIR/sites-available/$DOMAIN
+ echo "+ Created vhosts file at $APACHEDIR R/sites-available/$DOMAIN"
+
+ ## chown/chmod the vhosts files
+ sudo chown $USERNAME:$USERGROUP $APACHEDIR/sites-available/$DOMAIN
+ sudo chmod 775 $APACHEDIR/sites-available/$DOMAIN
+ echo "+ chowned/chmoded vhosts file"
+
+ sudo ln -s $APACHEDIR/sites-available/$DOMAIN $APACHEDIR/sites-enabled/
+
+ echo "+ Enabling the domain and reloading Apache:"
+ sudo a2ensite $DOMAIN
+ sudo service apache2 reload
+
+ echo "! Finished! Domain \"$DOMAIN\" was added"
+     
+}
+
+create_databases(){
+
+ Q1="CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+ Q2="CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';"
+ Q3="GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
+ Q4="FLUSH PRIVILEGES;"
+ SQL="${Q1}${Q2}${Q3}${Q4}"
+  
+ echo "Please enter root user MySQL password!"
+ read rootPassword
+ mysql -u root -p${rootPassword} -e "$SQL"
+     
+ echo "MySQL DB / User creation completed!"
+ echo " >> Host      : ${DB_HOST}"
+ echo " >> Database  : ${DB_NAME}"
+ echo " >> User      : ${DB_USER}"
+ echo " >> Pass      : ${DB_PASS}"
+ 
+
+}
+
+delete_old_domain(){
+     if [ -z $DOMAIN ]
+     then
+  	     echo "You did not include the domain ..."
+  	     exit 1
+     fi
+
+     sudo rm -rf /home/$USERNAME/$DOMAIN
+     sudo rm -rf $APACHEDIR/sites-available/$DOMAIN
+}
+
+
 menu(){
           echo -ne "
      --------Program create a new domain---------- 
@@ -215,26 +243,7 @@ menu(){
      esac 
 }
 
-create_databases(){
 
- Q1="CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
- Q2="CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';"
- Q3="GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
- Q4="FLUSH PRIVILEGES;"
- SQL="${Q1}${Q2}${Q3}${Q4}"
-  
- echo "Please enter root user MySQL password!"
- read rootPassword
- mysql -u root -p${rootPassword} -e "$SQL"
-     
- echo "MySQL DB / User creation completed!"
- echo " >> Host      : ${DB_HOST}"
- echo " >> Database  : ${DB_NAME}"
- echo " >> User      : ${DB_USER}"
- echo " >> Pass      : ${DB_PASS}"
- 
-
-}
 
 main(){
      install_service
